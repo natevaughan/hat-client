e026ab1760df18244b0cb821ff54c4581b3d71c7
Nate Vaughan <nvaughanguitar@yahoo.com>
2017-12-04 22:51:25 -0600

    pulls last 20 json

--- hat.go
+++ hat.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "net/http"
+    "io/ioutil"
+)
 
 func main() {
-   fmt.Printf("hello, world\n")
+
+    host := os.Getenv("HAT_HOST")
+    token := os.Getenv("HAT_TOKEN") 
+    if (len(host) == 0) {
+        fmt.Printf("you must have HAT_HOST set in your environment\n")
+        return
+    }
+    if (len(token) == 0) {
+        fmt.Printf("you must have HAT_TOKEN set in your environment\n")
+        return
+    }
+    client := &http.Client{}
+ 
+    req, err := http.NewRequest("GET", host + "message/previous/20", nil)
+
+    req.Header.Add("api-token", token)
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Printf(err.Error() + "\n")
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    fmt.Printf(string(body))
 }
